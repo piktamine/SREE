84988e9d77f75de323b871be0a27555c824acb09
piktamine <49212598+piktamine@users.noreply.github.com>
2021-06-11 21:43:24 -0500

    V0.46

--- routes/MisRutas/capturista.php
+++ routes/MisRutas/capturista.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\CapturistaController;
 //CAPTURISTA:
 Route::get('/capturista/captura',[CapturistaController::class,'capturar'])
 ->middleware('role:capturista')->name('capturar');//Capturar resultados (capturista)
-Route::post('/capturista/captura',[CapturistaController::class,'storecapturar'])->name('capturasubmit');//Capturar resultados (capturista)
-Route::get('/capturista/buscaru',[CapturistaController::class,'buscaru'])->name('cap_buscaru');//Capturar resultados (capturista)
+Route::post('/capturista/captura',[CapturistaController::class,'storecapturar'])->middleware('role:capturista')->name('capturasubmit');//Capturar resultados (capturista)
+Route::get('/capturista/buscaru',[CapturistaController::class,'buscaru'])->middleware('role:capturista')->name('cap_buscaru');//Capturar resultados (capturista)
 
